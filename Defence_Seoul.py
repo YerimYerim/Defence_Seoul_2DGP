d2b98f1e7cb1988554a5938ae359e733555f8d21
YerimYerim <vigvang_bip@naver.com>
2019-10-04 17:10:42 +0900

    배 띄우기 - 수정전 커밋
    수정하고 안돌아갈 수 있으니 커밋해놓고 안되면 다운받을 버전

--- Defence_Seoul.py
+++ Defence_Seoul.py
@@ -1,7 +1,61 @@
 from pico2d import *
+IMG_HEIGHT = 701
+IMG_WIDTH = 1648
 BackWIDTH = 961
 BackHIEGHT = 567
 tower_IMG_SIZE = 140
+Boat_IMG_SIZE = 135
+Tile_SIZE = 57
+x = 0
+y = 0
+
+class RECT:
+    left = 0
+    bot = 0
+    right = 0
+    top = 0
+
+class Tower:
+    Type = -1
+    Level = 1
+    R = RECT()
+
+
+class Boat:
+    hp = 1
+    speed = 1
+    type = 1    # 일반 1  보스 2
+    R = RECT()
+    R.bot = 7 * Tile_SIZE
+    R.left = -Tile_SIZE
+    R.right = 0
+    R.top = 8 * Tile_SIZE
+
+    def draw(self):
+        BackGround.clip_draw(32, 6, Boat_IMG_SIZE, Boat_IMG_SIZE ,
+                             (self.R.left+self.R.right) / 2, (self.R.top + self.R.bot) / 2, Tile_SIZE, Tile_SIZE)
+
+    def move(self):
+  #
+        if  self.R.right < Tile_SIZE * 2 - 2:
+            self.R.left += self.speed
+            self.R.right += self.speed
+        elif self.R.right == Tile_SIZE * 2 - 2 and self.R.bot > 6 * Tile_SIZE:
+            self.R.bot -= self.speed
+            self.R.top -= self.speed
+        elif self.R.right >= Tile_SIZE * 2 - 2 and self.R.bot == 6 * Tile_SIZE:
+            self.R.left += self.speed
+            self.R.right += self.
+        elif self.R.right >= Tile_SIZE * 2 - 2
+
+
+
+
+
+ #
+
+
+
 
 open_canvas(BackWIDTH, BackHIEGHT)
 
@@ -30,15 +84,17 @@ def handle_events():
     pass
 
 
+BackGround = load_image('resources.png')
+B = Boat()
 while running:
     clear_canvas()
 
-    if BackGround is None:
-        BackGround = load_image('resources.png')
-    BackGround.clip_draw(666, 708-583, BackWIDTH, BackHIEGHT, BackWIDTH/2 , BackHIEGHT/2 )
-
+    BackGround.clip_draw(666, 708-583, BackWIDTH, BackHIEGHT, BackWIDTH/2, BackHIEGHT/2 )
+    B.move()
+    B.draw()
     update_canvas()
     handle_events()
+    delay(0.02)
 
 close_canvas()
 
