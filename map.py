ced840ba3275a8338da92a648bce2c2e5edefb29
YerimYerim <vigvang_bip@naver.com>
2019-10-29 03:26:29 +0900

    Revert "충돌체크 구현, 맵 선택을위한 충돌박스"
    그리고 프레임워크 적용

--- map.py
+++ map.py
@@ -1,28 +1,11 @@
-
+from pico2d import *
+from Boat import *
+from setting import *
 from Tower import *
 
-tempRC = RECT()
-TileRect = RECT()
-class Map:
+class map:
     def __init__(self):
         self.Tiles = [[0] * 17 for i in range(10)]
-        self.type = 0
-        self.TileRow = [i * Tile_SIZE for i in range(19)]
-        self.TileCol = [i * Tile_SIZE for i in range(12)]
-
-
-    def select(self, x, y, type):
-        global tempRC, TileRect
-        tempRC = set(x, y, x+1, y+1)
-        for i in range(11):
-            for j in range(18):
-                TileRect = RECT().set(self.TileRow[j], self.TileCol[i], self.TileRow[j+1], self.TileCol[i+1])
-                if InterSectRECT(tempRC, TileRect):
-                    self.Tiles[i][j] = type
-        pass
-#map = Map()
-#for i in range(0,10):
-#    for j in range(0,17):
-#        TileRect = RECT()
-#        TileRect.set(map.TileRow[j], map.TileCol[i], map.TileRow[j + 1], map.TileCol[i + 1])
-#        print(TileRect.left , TileRect.right)
+        self.Img = load_image('Spritesheet\\resource.png')
+        self.Light = RECT()
+        self.Light.set()
