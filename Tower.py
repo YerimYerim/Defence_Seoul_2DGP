17a4a4394acb1e19fc33b39a76a8893a7c3fccb0
YerimYerim <vigvang_bip@naver.com>
2019-10-31 16:33:55 +0900

    타워설치 - 색별로 보여짐
    호호홓

--- Tower.py
+++ Tower.py
@@ -11,14 +11,16 @@ class Tower:
         self.angle = 0
         self.Boat_frame = 0
         self.type = -1       # 0- 불 1 - 냉기  2 - 전기 3 - 강등
+
         if Tower.Img is None:
             Tower.Img = load_image('Spritesheet\\resource.png')
             Tower.HP_font = load_font('font\\SeoulNamsanB.ttf', 13)
             print(" 그림 업로드")
 
     def draw(self):
-        self.Img.clip_composite_draw(13, 678 - 140, 142, 142, self.angle, ' ', self.R.left + Tile_SIZE / 2, BackHIEGHT - self.R.bot + Tile_SIZE / 2, Tile_SIZE, Tile_SIZE)
-   #     print("타워그림")
+            self.Img.clip_composite_draw(13 * self.type + 144 * self.type, IMG_HEIGHT - 144 - 16, 144, 144, self.angle, ' ', self.R.left + Tile_SIZE / 2, BackHIEGHT - self.R.bot + Tile_SIZE / 2, Tile_SIZE, Tile_SIZE)
+
+    #     print("타워그림")
 
     def install(self , area):
         area = RECT()
@@ -26,5 +28,5 @@ class Tower:
         pass
 
     def update(self):
-        self.angle += 0.1
+        self.angle += 0.01
         pass
