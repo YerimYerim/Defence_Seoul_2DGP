497d074b53ac724c658447962bc81fed394e3e01
YerimYerim <vigvang_bip@naver.com>
2019-11-03 23:38:30 +0900

    엥 버튼소리 로드 오류나넹

--- State_start.py
+++ State_start.py
@@ -5,19 +5,22 @@ from pico2d import *
 
 start_Img = None
 start_Sound = None
+button_sound = None
 name = "Start_state"
 def enter():
-    global start_Img , start_Sound
+    global start_Img , start_Sound , button_sound
     start_Img = load_image('Map\\start.png')
     start_Sound = load_music('sound\\대기.mp3')
     start_Sound.set_volume(128)
+    button_sound = load_wav('sound\\button.mp3')
+    button_sound.set_volume(50)
     print("재생")
     start_Sound.repeat_play()
 
 
 def exit():
-    global start_Img,start_Sound
-    del start_Img , start_Sound
+    global start_Img,start_Sound, button_sound
+    del start_Img , start_Sound , button_sound
 
 def pause():
     pass
@@ -27,10 +30,11 @@ def resume():
     pass
 
 def handle_events():
-    global running
+    global running,button_sound
     events = get_events()
     for event in events:
         if event.type == SDL_MOUSEBUTTONDOWN:
+            button_sound.play
             framework.change_state(State_main)
             print("메ㄴ인으로넘어감")
             pass
