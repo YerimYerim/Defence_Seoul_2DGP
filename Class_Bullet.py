4a1f59695d71501bce77e2e48d8a6a8d2b55c70b
YerimYerim <vigvang_bip@naver.com>
2019-11-04 01:42:36 +0900

    이닛함수

--- Class_Bullet.py
+++ Class_Bullet.py
@@ -6,13 +6,12 @@ import gameover_stage
 from Class_Tower import *
 from Class_Boat import *
 class Bullet :
-    def __init__(self):
+    def __init__(self , type):
         self.From = Tower()
         self.To = Boat()
-        
-
-
-
+        self.frame = 0
+        self.Type = -1
+        self.image = Image()
         pass
     def draw(self):
         pass
