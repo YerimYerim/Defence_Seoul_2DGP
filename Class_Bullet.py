9243ee660f244ee1e73acaccd09e0a8f3cf442bc
YerimYerim <vigvang_bip@naver.com>
2019-11-04 22:59:57 +0900

    포탄이미지 변경 , 클립드로우

--- Class_Bullet.py
+++ Class_Bullet.py
@@ -5,17 +5,37 @@ import State_start
 import gameover_stage
 from Class_Tower import *
 from Class_Boat import *
+
+
 class Bullet :
-    From = Tower()
-    To = Boat()
-    def __init__(self , FROM , TO ):
+    From = None
+    To = None
+    def __init__(self , FROM = Tower(), x, y):
         self.From = FROM
-        self.To = TO
-        self.Frame = 0
+        self.x, self.y = (self.From.Rectangle.left + self.From.Rectangle.right) / 2 , (self.From.Rectangle.top + self.From.Rectangle.bot) / 2
+        self.To = (x, y)
         self.Type = -1
-        self.Image = load_image()
+        self.Image = load_image('Spritesheet\\bullet.png')
         pass
     def draw(self):
+        if self.type is 0:
+            self.Image.clip_draw(0,0,44,44, self.x , self.y , 20, 20 )
+            pass
+        if self.type is 1:
+            self.Image.clip_draw(0,0,44,44, self.x , self.y , 20, 20 )
+            pass
+        if self.type is 2:
+            self.Image.clip_draw( )
+            pass
+        if self.type is 3:
+            self.Image.clip_draw( )
+            pass
+
+
         pass
     def update(self):
+
+
+
+
         pass
